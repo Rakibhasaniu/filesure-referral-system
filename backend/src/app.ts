4b5e5115b6024c64b59c47630263b8b9018f70ca
Rakib <rakibhasanoyoniu@gmail.com>
2025-10-24 15:13:32 +0600

    feat: add fly.toml configurations for production deployment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,7 +26,11 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      process.env.FRONTEND_URL || '',
+    ].filter(Boolean),
     credentials: true,
   }),
 );
